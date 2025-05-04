9099359ab2b07c1d692dcf51605b3c0a96c1acac
sharadsukhija <80167011+sharadsukhija@users.noreply.github.com>
2025-05-04 17:52:31 +0530

    Add files via upload

---
+++ tele.py
@@ -0,0 +1,43 @@
+init sys
+init os as qbitsys tree
+create switch from qiskit
+as alice qis and bob qis
+for envelope as network web
+create above function for qiskit
+for above line import newfile.py
+create above func as winsoundos()
+as qiskit connection _child process
+return above func(print) os 
+result network node() for qiskit
+visualise above qiskit sys as webos
+return result(quant.py) for clusys.py
+create qbit for child process
+return table for datalog as func(iter)
+import nse gtl for above func
+as simulator = future() func
+create bell from winsoundos for func(return)
+result above qiskit directory for data log
+as visualiser qiskit gate from el
+as print(result)
+print/define func()_line 1...n
+for quant q as qiskit from quant.py/newfile.py
+return result(bool)_future()
+return table log for db2-dx as ibm -u sharadsukhija
+print result(binascii) for func_bool
+return(result) as numpy
+print (mod_pandas ) for datalog
+return(])
+activate  bell from qiskit as network nodes
+for datalog  print (table)
+export above func() as qbit for db2-dx
+return(solve) f4om mod_scipy,numpy,abcpy
+as value varchar from index for above
+create quant bit from ml.py as above
+return(func)
+create web hook as clusys.py from above
+define port from above func(iter)
+return(bool)
+return print for ev.py
+create streamfunc() as iostream from mod_python
+create envelope for above as func(bool) binascii from nse gtl
+retrun(log)
